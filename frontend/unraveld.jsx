4eba1e2f39a2225c7d7acc77938bf55974a1661d
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-18 14:18:42 -0700

    adding reducers store and session actions

--- frontend/unraveld.jsx
+++ frontend/unraveld.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>UNRAVELD</h1>, root);
 });
