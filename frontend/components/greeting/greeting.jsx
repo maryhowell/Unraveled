8fe78b3991c2bb5495c2d5c94b7169e598ec8cad
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-18 17:28:59 -0700

    adding more wireframes

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -5,14 +5,13 @@ import { Link } from 'react-router-dom';
 const sessionLinks = () => (
   <nav className="login-signup">
     <Link to="/login">Login</Link>
-    &nbsp;or&nbsp;
     <Link to="/signup">Sign up!</Link>
   </nav>
 );
 
 const personalGreeting = (currentUser, logout) => (
 	<hgroup className="header-group">
-    <h2 className="header-name">Hi, {currentUser.first_name}!</h2>
+    <h2 className="header-name">Hi, {currentUser.email}!</h2>
     <button className="header-button" onClick={logout}>Log Out</button>
 	</hgroup>
 );
