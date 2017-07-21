4a67d547fc6a0bbd6c106ac9d60c4923ee1e2e2e
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-20 18:00:59 -0700

    working on buttons

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -42,7 +42,7 @@ const signedInGreeting = (currentUser, logout) => (
       </div>
 
       <div>
-        <span className="usergreeting" >Hi, {currentUser.email}!</span>
+        <span className="user" >Hi, {currentUser.email}!</span>
         <button className="signoutbutton" onClick={logout}>Sign Out</button>
       </div>
 
