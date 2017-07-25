482fce8ac48bf889033bede2df6e56f4e2d86085
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-25 15:42:44 -0700

    adding button to home page

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -39,12 +39,37 @@ const signedInGreeting = (currentUser, logout) => (
         <Link to="/">
           UnRavled
         </Link>
-      </div>
 
-      <div>
-        <span className="user" >Hi, {currentUser.first_name}!</span>
-        <button className="signoutbutton" onClick={logout}>Sign Out</button>
       </div>
+      <Link className="allpatternsbutton" to="/patterns">
+      Shop All Patterns
+      </Link>
+      <nav className='signout-nav'>
+     <ul>
+       <li className='user'>
+         <ul>
+
+           <li>{`${currentUser.first_name}`}</li>
+         </ul>
+       </li>
+       <li>
+         <Link to='/'>
+           <button className='signoutbutton' onClick={ logout }>SignOut</button>
+         </Link>
+       </li>
+       <li>
+         <Link className='cart-link' to='/cart'>
+           <ul className='cart'>
+             <li>
+               <i className='fa fa-shopping-cart' name='fa-shopping-cart' aria-hidden='true' />
+             </li>
+             <li>Cart</li>
+           </ul>
+         </Link>
+       </li>
+
+     </ul>
+   </nav>
     </nav>
   </header>
 
@@ -58,3 +83,18 @@ const Greeting = ({ currentUser, logout, login }) => (
 );
 
 export default Greeting;
+//
+// <li>
+//   <i className='fa fa-user-circle-o' name='fa-user-circle-o' aria-hidden='true' />
+// </li>
+
+// <li>
+//   <Link to='/purchased_items'>
+//     <ul className='purchases'>
+//       <li>
+//         <i className='fa fa-credit-card-alt' name='fa-credit-card-alt' aria-hidden='true' />
+//       </li>
+//       <li>Purchases</li>
+//     </ul>
+//   </Link>
+// </li>
