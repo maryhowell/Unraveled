96209df133ff8a0189fdc724c6b7c7e284209cbe
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-25 17:35:29 -0700

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
     resources :patterns, only: [:create, :index, :show]
+    resources :carts, only: [:create, :index, :update]
+    resources :cart_items, only: [:index, :create, :update, :destroy]
   end
 
    root "static_pages#root"
